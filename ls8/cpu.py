e621f47be47917cb85a9ca9d9d4623f1b646f60d
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-04-21 15:58:59 -0400

    mult working, dynamic input from cmd line

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,22 +8,23 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [None] * 256
-        self.registers = [None] * 8
+        self.reg = [None] * 8
         self.instructions = {
             0b10000010: self.ldi,
             0b01000111: self.prn,
-            0b00000001: self.hlt
+            0b00000001: self.hlt,
+            0b10100010: self.mul,
         }
 
     def prn(self, op1, op2):
-        print(self.registers[op1])
+        print(self.reg[op1])
         return (2, True)
     
     def hlt(self, op1, op2):
         return (0, False)
     
     def ldi(self, op1, op2):
-        self.registers[op1] = op2
+        self.reg[op1] = op2
         return (3, True)
 
     def ram_read(self, address):
@@ -32,36 +33,37 @@ class CPU:
     def ram_write(self, address, value):
         self.ram[address] = value
 
+    def mul(self, reg_a, reg_b):
+        self.alu('MUL', reg_a, reg_b)
+        return (3, True)
+
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        file_name = sys.argv[1]
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        with open(file_name) as f:
+            for line in f:
+                line_string = line.split('#')[0].strip()
+                if line_string == '':
+                    continue
+                inst = int(line_string, 2)
+                
+                self.ram[address] = inst
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == 'MUL':
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
 
     def trace(self):
         """
@@ -82,6 +84,7 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+        
 
     def run(self):
         """Run the CPU."""
